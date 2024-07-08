9cbe3231537613f7acdc29c826426ffc6825cd94
@imam <imamaqrom@gmail.com>
2024-07-08 23:03:18 +0700

    intergration email after register and after checkout

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests\User\Checkout\Store;
 use App\Models\Camp;
 use Auth;
+use Mail;
+use App\Mail\Checkout\AfterCheckout;
 
 class CheckoutController extends Controller
 {
@@ -47,7 +49,7 @@ class CheckoutController extends Controller
     public function store(Store $request, Camp $camp)
     {
         // return $camp;
-        return $request->all();
+        // return $request->all();
 
         // mapping request data
         $data = $request->all();
@@ -63,6 +65,9 @@ class CheckoutController extends Controller
 
         // create checkout
         $checkout = Checkout::create($data);
+
+        // sending email
+        Mail::to(Auth::user()->email)->send(new AfterCheckout($checkout));
 
         return redirect(route('checkout.success'));
 
@@ -116,5 +121,10 @@ class CheckoutController extends Controller
     public function success()
     {
         return view('checkout.success');
+    }
+
+    public function invoice(Checkout $checkout)
+    {
+        return $checkout;
     }
 }
